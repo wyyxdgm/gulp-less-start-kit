0afd534894c13e431f03ad7b8b0970919b5709e6
guimao dai <wyyxdgm@163.com>
2019-11-25 09:09:42 +0800

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,9 @@ let autoprefixer = require('autoprefixer');
 let clean = require('gulp-clean');
 let changed = require("gulp-changed");
 var browserSync = require('browser-sync').create();
-let concatCss = require('gulp-concat-css')
+let concatCss = require('gulp-concat-css');
+let watch = require('@jindasong/gulp-watch');
+
 let useChanged = false;
 let CONFIG = require('./config');
 let compile = require('./lib/compile.js')
@@ -26,16 +28,22 @@ let DIST = CONFIG.dist;
  * replace: video,files,img
  * build: html
  */
-gulp.task('default', ['build:html', 'css', 'copy']);
-gulp.task('watch', ['default', 'watchhtml', 'watchi18n', 'watchcss', 'watchcopy', 'watchdev', 'serve']);
 
-gulp.task('watchi18n', function() {
-  gulp.watch(['i18n/*.js'], function(event) {
+gulp.task('build:html', function(cb) {
+  fs.readdir('./src/pages', function(err, files) {
+    files.forEach(file => file.indexOf('.html') > 0 && buildPages(file.replace('.html', '')));
+    cb();
+  })
+})
+
+gulp.task('watchi18n', function(cb) {
+  watch(['i18n/*.js'], function(event) {
     var paths = watchPath(event, 'i18n', 'i18n');
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     gutil.log('Dist ' + paths.distPath.replace('i18n/', '').replace(/\.\w+$/, '/index.html'));
     gulp.run('build:html');
   });
+  cb();
 });
 
 
@@ -68,14 +76,14 @@ let buildStyle = _.throttle((src, needCssmin, base, dist) => {
   return line;
 }, 100);
 
-gulp.task('watchdev', function() {
+gulp.task('watchdev', function(cb) {
 
   if (!fs.existsSync('lab')) fs.mkdirSync('lab');
   if (!fs.existsSync('lab/src')) fs.mkdirSync('lab/src');
   if (!fs.existsSync('lab/dist')) fs.mkdirSync('lab/dist');
-  gulp.watch(['lab/src/**'], _.throttle(function(event) {
+  watch(['lab/src/**'], _.throttle(function(event) {
     var paths = watchPath(event, 'lab/src/', 'lab/dist/')
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     // html
     if (/\.html$/.test(paths.srcPath)) {
       if ('deleted' == event.type) return cleanFiles([paths.distPath, paths.distPath.replace(/\.html$/, '.*.js')]);
@@ -89,34 +97,36 @@ gulp.task('watchdev', function() {
       compile.buildCss(paths.srcPath, paths.distPath);
     }
   }, 500));
+  cb();
 });
 
-gulp.task('watchcss', function() {
-  gulp.watch(['src/css/*.css'], function(event) {
+gulp.task('watchcss', function(cb) {
+  watch(['src/css/*.css'], function(event) {
     var paths = watchPath(event, 'src/css', 'dist/css')
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     gutil.log('Dist ' + paths.distPath)
     buildStyle(paths.srcPath);
   })
   Object.keys(CONFIG.i18n).forEach((lang) => {
-    gulp.watch(['src/css/*.css', `src/i18n/${lang}/css/*.css`], function(event) {
+    watch(['src/css/*.css', `src/i18n/${lang}/css/*.css`], function(event) {
       var paths = watchPath(event, `src/i18n/${lang}/css`, `dist/i18n/${lang}/css`)
-      gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+      gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
       gutil.log('Dist ' + paths.distPath)
       buildStyle(['src/css/*.css'], CONFIG.minCss, `src/i18n/${lang}/css`, `i18n/${lang}/css`);
     })
   });
-
+  cb();
 })
 
-gulp.task('css', function() {
+gulp.task('css', function(cb) {
   buildStyle('src/css/*.css', CONFIG.minCss);
   Object.keys(CONFIG.i18n).forEach((lang) => {
     buildStyle(['src/css/*.css', `src/i18n/${lang}/css/*.css`], CONFIG.minCss, `src/i18n/${lang}/css`, `i18n/${lang}/css`);
+    cb();
   });
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', function(cb) {
   browserSync.init({
     watch: true,
     files: ['dist/**'],
@@ -126,13 +136,14 @@ gulp.task('serve', function() {
       index: "zh/index.html"
     }
   });
+  cb();
 });
 
 
-gulp.task('clean', () => cleanFiles('dist/'));
+gulp.task('clean', (cb) => cleanFiles('dist/') || cb());
 
-gulp.task('watchcopy', function() {
-  gulp.watch([
+gulp.task('watchcopy', function(cb) {
+  watch([
     'src/fonts/**',
     'src/files/**',
     'src/img/**',
@@ -142,26 +153,26 @@ gulp.task('watchcopy', function() {
   ], function(event) {
     var paths = watchPath(event, 'src/', 'dist/')
 
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
-    gutil.log('Dist ' + paths.distPath)
+    gutil.log('copy', paths.srcPath.replace(/\\/g, '/'), paths.distPath.replace(/\\/g, '/'))
 
     gulp.src(paths.srcPath)
       .pipe(gulp.dest(paths.distDir))
   })
-  gulp.watch([
+  watch([
     'oss/**'
   ], function(event) {
     var paths = watchPath(event, 'oss/', 'dist/')
 
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     gutil.log('Dist ' + paths.distPath)
 
     gulp.src(paths.srcPath)
       .pipe(gulp.dest(paths.distDir))
   })
+  cb();
 })
 
-gulp.task('copy', function() {
+gulp.task('copy', function(cb) {
   gulp.src(['src/fonts/**',
       'src/files/**',
       'src/img/**',
@@ -185,26 +196,22 @@ gulp.task('copy', function() {
       base: 'oss/'
     })
     .pipe(gulp.dest('dist/'))
+  cb();
 });
 
-gulp.task('watchhtml', function() {
-  gulp.watch(['src/pages/*.html'], function(event) {
+gulp.task('watchhtml', function(cb) {
+  watch(['src/pages/*.html'], function(event) {
     var paths = watchPath(event, 'src/pages/', 'dist/')
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     gutil.log('Dist ' + paths.distPath)
-    buildPages(paths.distPath.replace('dist/', '').replace('.html', ''))
+    buildPages(paths.distPath.replace(/dist(\/|\\)/, '').replace('.html', ''))
   })
-  gulp.watch(['src/pages/template/*.html'], ['build:html'], function(event) {
+  watch(['src/pages/template/*.html'], gulp.series('build:html'), function(event) {
     var paths = watchPath(event, 'src/pages/template/', 'dist/')
-    gutil.log(gutil.colors.green(event.type) + ' ' + paths.srcPath)
+    gutil.log(gutil.colors.green(event.type || 'changed') + ' ' + paths.srcPath)
     gutil.log('Dist ' + paths.distPath)
   })
-})
-
-gulp.task('build:html', function() {
-  fs.readdir('./src/pages', function(err, files) {
-    files.forEach(file => file.indexOf('.html') > 0 && buildPages(file.replace('.html', '')));
-  })
+  cb();
 })
 
 function buildPages(htmlName) {
@@ -270,7 +277,7 @@ function buildPages(htmlName) {
 // });
 
 
-gulp.task('i18n2xls', function() {
+gulp.task('i18n2xls', function(cb) {
   let i18nJson = {};
   let keys = {};
   Object.keys(CONFIG.i18n).forEach((lang) => {
@@ -282,10 +289,11 @@ gulp.task('i18n2xls', function() {
   });
   fs.writeFileSync('lab/dist/i18n.xls', Object.keys(CONFIG.i18n).join('\t') + '\n' + _.values(keys).join('\n'));
   gutil.log('convert i18n data to xls: lab/dist/i18n.txt');
+  cb();
 });
 
 
-gulp.task('xls2i18n', function() {
+gulp.task('xls2i18n', function(cb) {
   let i18nJson = {},
     i18nJsonOld = {};
   let keys = {};
@@ -307,15 +315,21 @@ gulp.task('xls2i18n', function() {
     fs.writeFileSync(path.join(__dirname, `./i18n/${lang}.js`), 'module.exports = ' + JSON.stringify(i18nJson[lang], null, '\t'));
     gutil.log('convert xls to i18n data!!');
   });
+  cb();
 
 });
 
 
-gulp.task('deploy_dev', function() {
+gulp.task('deploy_dev', function(cb) {
   const { deploy } = require('sftp-sync-deploy');
   deploy(CONFIG.dev.sftp.config, CONFIG.dev.sftp.options).then(() => {
     console.log('deploy dev success!');
+    cb();
   }).catch(err => {
     console.error('error! ', err);
+    cb();
   });
 })
+
+gulp.task('default', gulp.series('build:html', 'css', 'copy', (cb) => cb()));
+gulp.task('watch', gulp.series('default', 'watchhtml', 'watchi18n', 'watchcss', 'watchcopy', 'watchdev', 'serve'));
